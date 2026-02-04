5166602e51c22fe386979a9a0ec34fe2e2bc6b8b
Vegard Skogstad <skogstadv@gmail.com>
2026-02-04 09:46:15 +0000

    Tried removing json dumps in classifiers to avoid formatting mistakes

--- cs336-basics/scripts/sample_dataset.py
+++ cs336-basics/scripts/sample_dataset.py
@@ -3,11 +3,11 @@ from transformers import AutoTokenizer
 import numpy as np
 
 tokenizer = AutoTokenizer.from_pretrained("gpt2")
-
+print("Training data:")
 data2 = np.fromfile("data/train96.bin", dtype=np.uint16)
-print(tokenizer.decode(data2[:100]))
-print("\n\n\n\n\nValidation data")
+print(tokenizer.decode(data2[:1000]))
+print("\n______________________________________________________________________________________________\n\n\n\nValidation data:")
 
 data = np.fromfile("data/tokenized_paloma_c4_100_domains_validation.bin", dtype=np.uint16)
 
-print(tokenizer.decode(data[:100]))
+print(tokenizer.decode(data[:1000]))
